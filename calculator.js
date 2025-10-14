c13dbef037a99111af70017a41287ec7fc4a2df3
rydzn27 <reydizon27@gmail.com>
2025-10-14 22:04:48 +0800

    Create the operate() and reorder the buttons in html.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
 function add(a, b) {
     return a + b;
 }
@@ -11,5 +15,20 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return "Error";
+    }
     return a / b;
+}
+
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "x") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
 }
